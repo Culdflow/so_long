1fbd2eb8d4e53c6500cbafc4f84ef1af59b90b09
Dimitri Feve <dfeve@c1r4p1.42nice.fr>
2025-01-07 01:36:17 +0100

    fixed everything ahaha

--- error/error.c
+++ error/error.c
@@ -6,7 +6,7 @@
 /*   By: dfeve <dfeve@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 18:55:37 by dfeve             #+#    #+#             */
-/*   Updated: 2024/12/26 22:55:10 by dfeve            ###   ########.fr       */
+/*   Updated: 2025/01/07 01:35:41 by dfeve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	error(char *text, t_mlx *mlx)
 {
 	if (mlx)
 		exit_game(mlx);
+	write(2, "\e[0;31m[ERROR] : \033[0m", 22);
 	perror(text);
-	exit(0);
+	exit(1);
 }

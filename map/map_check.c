cec625738b603e52a8ffa28866261aee592da388
Dimitri Feve <dfeve@c1r4p1.42nice.fr>
2025-01-07 01:48:15 +0100

    norminette again

--- map/map_check.c
+++ map/map_check.c
@@ -6,7 +6,7 @@
 /*   By: dfeve <dfeve@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 20:44:11 by dfeve             #+#    #+#             */
-/*   Updated: 2025/01/07 01:41:48 by dfeve            ###   ########.fr       */
+/*   Updated: 2025/01/07 01:47:23 by dfeve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,21 +70,20 @@ void	check_map(t_map *map, t_mlx *mlx)
 
 	pos_exit = vec2(0, 0);
 	open_zero = vec2(0, 0);
-	cursor = vec2(0, 0);
-	while (map->map[cursor.y])
+	cursor = vec2(0, -1);
+	while (map->map[cursor.y++])
 	{
 		cursor.x = 0;
 		while (map->map[cursor.y][cursor.x])
 		{
 			if (check_tile(map, map->map[cursor.y][cursor.x],
-								cursor, &pos_exit) == -1)
+				cursor, &pos_exit) == -1)
 				error("unsupported character in map", mlx);
 			if (map->map[cursor.y][cursor.x] == 'B')
 				open_zero.x++;
 			if (map->map[cursor.y][cursor.x++] == '0')
 				open_zero.y++;
 		}
-		cursor.y++;
 	}
 	if (map->collectible_list == NULL || !compare_vec2(pos_exit, vec2(1, 1))
 		|| open_zero.x > 0 || open_zero.y <= 0
